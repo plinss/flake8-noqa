915b4e71224862ad073ab402d4369562996b559d
Peter Linss <peter@linss.com>
2022-08-01 20:27:09 -0700

    Update dependencies to allow flake8 5.x releases Fixes #16

--- setup.py
+++ setup.py
@@ -21,14 +21,14 @@ setuptools.setup(
 	package_data={'flake8_noqa': ['py.typed']},
 
 	install_requires=[
-		'flake8>=3.8.0,<5.0',
+		'flake8>=3.8.0,<6.0',
 		'importlib_metadata>=4.0.0,<5.0.0;python_version<"3.8.0"',
 		'typing_extensions>=3.7.4.2',
 	],
 	extras_require={
 		'dev': [
 			'mypy',
-			'flake8',
+			'flake8>=3.8.0,<5.0',
 			'flake8-annotations',
 			'flake8-bandit',
 			'flake8-bugbear',
