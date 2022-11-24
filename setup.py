eeefd0ffc859188d9910b0bcd8818e6d70c4c4cd
Ville Skyttä <ville.skytta@iki.fi>
2022-11-24 08:20:50 +0200

    Update flake8 dependency upper bound to 7.0

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setuptools.setup(
 	package_data={'flake8_noqa': ['py.typed']},
 
 	install_requires=[
-		'flake8>=3.8.0,<6.0',
+		'flake8>=3.8.0,<7.0',
 		'importlib_metadata>=4.0.0,<5.0.0;python_version<"3.8.0"',
 		'typing_extensions>=3.7.4.2',
 	],
